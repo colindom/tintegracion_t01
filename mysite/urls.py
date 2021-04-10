8074e36f8d35a912327e8b6649938463f1216c85
unknown <jpdominguez@uc.cl>
2021-04-10 12:34:50 -0400

    Basic redirect

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('walt/', include('walt.urls')),
+    path('', include('walt.urls')),
     path('admin/', admin.site.urls),
 ]
